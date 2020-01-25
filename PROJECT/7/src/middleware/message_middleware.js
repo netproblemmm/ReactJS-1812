10629525096359c094b9f14352393daa1b7fa116
kellolo <87kell@gmail.com>
2020-01-25 11:00:56 +0200

    7

---
+++ PROJECT/7/src/middleware/message_middleware.js
@@ -0,0 +1,13 @@
+import { SEND_MESSAGE, sendMessage } from '../actions/message_actions.js'
+
+export default store => next => (action) => {
+    switch (action.type) {
+        case SEND_MESSAGE:
+            if (action.sender === 'it') {
+                setTimeout(() => store.dispatch(sendMessage(Object.keys(store.getState().messageReducer.messages).length + 1,
+                        'Не приставай ко мне, я робот!', 'bot', action.chatId)), 1000)
+            }
+    }
+    return next(action)
+ }
+ 
