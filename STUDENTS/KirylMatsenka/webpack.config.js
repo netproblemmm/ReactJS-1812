e5c43384fc0224c0a6f1d4e074a0fbf89b161fd4
KirylJazzSax <matsak_92@mail.ru>
2020-01-28 12:08:07 +0300

    Настраиваем работу

--- STUDENTS/KirylMatsenka/webpack.config.js
+++ STUDENTS/KirylMatsenka/webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: [
+              "@babel/plugin-transform-runtime",
               [
                 "@babel/plugin-proposal-class-properties",
                 {
