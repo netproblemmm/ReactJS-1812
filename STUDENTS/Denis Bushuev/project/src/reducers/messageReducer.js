df5b59924289c57249cb6c5a53512f5bd9c1bf9d
Jestric-sys <Poltos_94@mail.ru>
2020-01-29 14:26:18 +0300

    Выполнение ДЗ по 7 уроку

--- STUDENTS/Denis Bushuev/project/src/reducers/messageReducer.js
+++ STUDENTS/Denis Bushuev/project/src/reducers/messageReducer.js
@@ -1,11 +1,17 @@
 import update from 'react-addons-update';
-import { SEND_MESSAGE } from '../actions/messageActions.js';
+import { 
+    SEND_MESSAGE,
+    START_MESSAGES_LOADING,
+    SUCCESS_MESSAGES_LOADING,
+    ERROR_MESSAGES_LOADING, 
+} from '../actions/messageActions.js';
 
 const initialStore = {
     messages: {
-        1: { text: "Привет!", sender: 'bot' },
-        2: { text: "Здравствуйте!!!!", sender: 'bot' }
+        1: { text: "", sender: 'bot' },
+        2: { text: "", sender: 'bot' }
     },
+    isLoading: false,
 };
 
 export default function messageReducer(store = initialStore, action) {
@@ -21,6 +27,27 @@ export default function messageReducer(store = initialStore, action) {
                     }
                 }
             })
+        }
+        case START_MESSAGES_LOADING: {
+            return update(store, {
+               isLoading: { $set: true },
+            });
+        }
+        case SUCCESS_MESSAGES_LOADING: {
+            const messages = {};
+            action.payload.forEach(msg => {
+                const { text, sender } = msg;
+                messages[msg.id] = { text, sender };
+            });
+            return update(store, {
+                messages: { $set: messages },
+                isLoading: { $set: false },
+            });
+        }
+        case ERROR_MESSAGES_LOADING: {
+            return update(store, {
+                isLoading: { $set: false },
+            });
         }
         default: 
             return store;
